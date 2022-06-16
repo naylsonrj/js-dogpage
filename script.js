584913a5d1f6c4fcc6dfd15b822bd1685c743a41
Naylson Costa <naylsonrj@gmail.com>
2022-06-16 18:39:27 -0300

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const link = document.querySelectorAll("[href^='#']");
+console.log(link);
+
+const img = document.querySelectorAll("img");
+console.log(img);
